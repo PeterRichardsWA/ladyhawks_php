c88a7f897b962ccbea6bfc36391abadbef6c24ba
PeterRichardsWA <the.peter.richards@gmail.com>
2015-10-19 13:47:28 -0700

    working final

--- index.php
+++ index.php
@@ -3,24 +3,62 @@
 <head>
 	<meta charset="UTF-8">
 	<title>LadyHawks Wreath Sales Fundraiser</title>
+	<link rel="stylesheet" type="text/css" href="hawkstyles.css">
 </head>
 <body>
 <div id='wrapper'>
-	<div id='page1'>
+	<div id='head'>
+		<h2>FederalWay.com Presents:</h2>
+	</div>
+
+	<div id='welcome'>
+		<h3>Washington LadyHawks Wreath Sales 2015 - Orders due by November 14, 2015<br>
+		<p>Alexandra and Brooklyn Richards are local young ladies playing for the Washington Ladyhawks Fastpitch organization. 
+		These two talented softball players are raising funds for the 2015 season to be able to compete at the highest level 
+		of fastpitch 12u travel ball in Washington State.  The Washington LadyHawks is the best fastpitch organization, in existence for 
+		over 20 years, having sent a large number of players on commital to local division 1 programs.
+		<p>Brook and Lexi are asking that you help support their efforts by purchasing one or more of their holdiay wreaths.
+		If you have a larger organization, we can accomodate these types of purchases as well.
+
+		<div id='payment'>
+		Email us your order (item name, size, count of items, name and address with phone number) to
+		<p class='large'><b><u>the(dot)peter(dot)richards@gmail.com</u></b><br>(replace (dot) with "." - trying to avoid spam bots).</p>
+
+		Send your check with total amount of your order, made out to "Washington LadyHawks Fastpitch Organization" to:<br>
+		<p class='large'>
+		LadyHawks Wreaths<br>
+		33433 East Lake Holm DR SE<br>
+		Auburn, WA 98092<br>
+		</p>
+		We will fill out the paperwork and email you a copy of your completed invoice with a copy of the check and tax ID number for your records.
+		</div>
+
+		<p><b>ALL purchases are tax deductible through the LadyHawks program as a 501c3 non-profit</b>. We thank you for your generous support!
+	</div>
+
+	<div class='page'>
 		<h3>Brook and Lexi's Wreath Sales Catalog Page 1:<br>
-		<img src="images/wreath_page_1_resize.png" border=0 alt='page 1 wreath catalog'>
+		<img src="./images/wreath_catalog_1_resize.png" border=0 alt='page 1 wreath catalog'>
 	</div>
 
-	<div id='page2'>
+	<div class='page'>
 		<h3>Brook and Lexi's Wreath Sales Catalog Page 2:<br>
-		<img src="images/wreath_page_2_resize.png" border=0 alt='page 2 wreath catalog'>
+		<img src="./images/wreath_catalog_2_resize.png" border=0 alt='page 2 wreath catalog'>
 	</div>
 
-	<div id='page3'>
+	<div class='page'>
 		<h3>Brook and Lexi's Wreath Sales Catalog Page 3:<br>
-		<img src="images/wreath_page_3_resize.png" border=0 alt='page 3 wreath catalog'>
+		<img src="./images/wreath_catalog_3_resize.png" border=0 alt='page 3 wreath catalog'>
 	</div>
 
+	<div id='video'>
+		<p>Click for a friendly message from the girls...</p>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/rXVGZFpe1AM" frameborder="0" allowfullscreen></iframe>
+	</div>
+
+</div>
+<div id="footer">
+	<p>Copyright 2015 &copy; Washington LadyHawks Fastpitch Organization</p>
 </div>
 </body>
 </html>
