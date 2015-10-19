83c87390dfe174a7c29a597cc088f5e839e7d142
PeterRichardsWA <the.peter.richards@gmail.com>
2015-10-19 10:44:13 -0700

    second full

---
+++ README.md
@@ -0,0 +1,7 @@
+###############################
+
+10/19/2015
+Website simple version for raising funds for ladyhawks
+
+peter richards
+Brook and Lexi Richards
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>LadyHawks Wreath Sales Fundraiser</title>
+</head>
+<body>
+<div id='wrapper'>
+	<div id='page1'>
+		<h3>Brook and Lexi's Wreath Sales Catalog Page 1:<br>
+		<img src="images/wreath_page_1_resize.png" border=0 alt='page 1 wreath catalog'>
+	</div>
+
+	<div id='page2'>
+		<h3>Brook and Lexi's Wreath Sales Catalog Page 2:<br>
+		<img src="images/wreath_page_2_resize.png" border=0 alt='page 2 wreath catalog'>
+	</div>
+
+	<div id='page3'>
+		<h3>Brook and Lexi's Wreath Sales Catalog Page 3:<br>
+		<img src="images/wreath_page_3_resize.png" border=0 alt='page 3 wreath catalog'>
+	</div>
+
+</div>
+</body>
+</html>
